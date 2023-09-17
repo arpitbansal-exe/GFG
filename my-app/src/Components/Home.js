7c521eeab78f43162846eae2db1722a747fbe64c
Madhur <madhurpatil73@gmail.com>
2023-09-17 22:59:02 +0530

    Text part

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+import './Styles/Home.css';
+
 export default function Home() {
   return (
     <div>
-        <Navbar />
-        <h1>Home</h1>
+      <Navbar />
+      <div class="wrapper">
+        <p class="line__1">Conquering <span class="green-text">DSA.</span></p>
+        <p class="line__2">Creating with <span class="green-text">Web-Dev.</span></p>
+        <p class="line__3">Daily <span class="green-text">Tech-Bytes.</span></p>
+        <p class="line__4">Peer to Peer <span class="green-text">Learning.</span></p>
+      </div>
     </div>
-  )
+  );
 }
