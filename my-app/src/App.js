829f22b4237e558a0746bf6e7064d1941725bd08
Arpit bansal <arpitbansal2304@gmail.com>
2023-09-17 05:36:45 +0530

    "navbar and other pages"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Home from "./Components/Home";
 import NoPage from "./Components/NoPage";
 import Team from "./Components/Team";
 import Login from "./Components/Login";
+import About from "./Components/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
           <Route index element={<Home />} />
           <Route exact path="home" element={<Home />} />
           <Route exact path="team" element={<Team />} />
+          <Route exact path="about" element={<About />} />
           <Route exact path="login" element={<Login />} />
           <Route path="*" element={<NoPage />} />
       </Routes>
--- my-app/src/Components/Team.js
+++ my-app/src/Components/Team.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 export default function Team() {
   return (
     <div>
+      <Navbar />
         <h1>Team</h1>
     </div>
 
